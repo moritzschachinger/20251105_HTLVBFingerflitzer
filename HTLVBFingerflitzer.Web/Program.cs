9bd44cb7e1c5218f541aa40874696d6b78dd7ad0
Moritz Schachinger <M.Schachinger@htlvb.at>
2026-01-21 13:55:35 +0100

    Commit 20260121

--- HTLVBFingerflitzer.Web/Program.cs
+++ HTLVBFingerflitzer.Web/Program.cs
@@ -1,6 +1,10 @@
+
 using HTLVBFingerflitzer.Web.Components;
+using Microsoft.Identity.Web;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
---
+++ create-webapp1.ps1
@@ -0,0 +1,135 @@
+
+az login
+$UserName = ((az ad signed-in-user show | ConvertFrom-Json).userPrincipalName -replace '@.*$','' -replace '\W','').ToLower()
+$GithubRepositoryName = "moritzschachinger/20251105_HTLVBFingerflitzer"
+ 
+ 
+az group create --name rg-fingerflitzer --location swedencentral | Out-Null
+
+
+# TODO
+# App registration mit az cli
+# * Reply url, Secret
+# * Infos anzeigen (ClientId, TenantId, Instance, ResponseType)
+# * Client secret mit dotnet cli setzen
+# * Client secret in Azure web app config setzen
+# ============================
+# App Registration erstellen
+# ============================
+
+
+
+az appservice plan create `
+  --name asp-fingerflitzer `
+  --sku P0V3 `
+  --is-linux `
+  --resource-group rg-fingerflitzer | Out-Null
+ 
+az webapp create `
+  --name wa-fingerflitzer-$UserName `
+  --runtime DOTNETCORE:8.0 `
+  --assign-identity `
+  --https-only true `
+  --public-network-access Enabled `
+  --plan asp-fingerflitzer `
+  --resource-group rg-fingerflitzer | Out-Null
+ 
+$SubscriptionId = (az account show | ConvertFrom-Json).id
+ 
+$servicePrincipalSecret = az ad sp create-for-rbac `
+  --name "gh-action-to-deploy-fingerflitzer-webapp-schm" `
+  --role contributor `
+  --scopes /subscriptions/$SubscriptionId/resourceGroups/rg-fingerflitzer/providers/Microsoft.Web/sites/wa-fingerflitzer-$UserName `
+  --json-auth | Out-String
+ 
+gh auth login
+ 
+$servicePrincipalSecret | gh secret set AZURE_CREDENTIALS `
+--repo $GithubRepositoryName
+ 
+az webapp deployment slot create `
+--name wa-fingerflitzer-$UserName `
+--resource-group rg-fingerflitzer `
+--slot staging
+ 
+az webapp config appsettings set `
+ --settings "DailyChallenge__Type=static-text" "DailyChallenge__StaticText=Hi from Azure Web App!" `
+ --slot staging `
+ --name wa-fingerflitzer-$UserName `
+ --resource-group rg-fingerflitzer
+ 
+gh workflow run publish-fingerflitzer-web-app.yml `
+ --repo $GithubRepositoryName
+ 
+# # Allow access from web app to database
+# # see https://learn.microsoft.com/en-us/azure/app-service/tutorial-connect-msi-azure-database
+# az extension add --name serviceconnector-passwordless --upgrade
+# az webapp connection create postgres-flexible `
+#   --connection fingerflitzer_webapp `
+#   --resource-group rg-fingerflitzer `
+#   --name wa-fingerflitzer-$UserName `
+#   --target-resource-group rg-fingerflitzer `
+#   --server db-fingerflitzer-$UserName `
+#   --database fingerflitzer `
+#   --system-identity `
+#   --client-type dotnet | Out-Null
+ 
+# az extension add --name rdbms-connect
+# $User = az ad signed-in-user show | ConvertFrom-Json
+# $AccessToken = az account get-access-token --resource-type oss-rdbms | ConvertFrom-Json
+# az postgres flexible-server execute `
+#   --querytext "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO `"aad_beer4me_webapp`";GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO `"aad_beer4me_webapp`";" `
+#   --database-name fingerflitzer `
+#   --admin-user $User.userPrincipalName `
+#   --admin-password $AccessToken.accessToken `
+#   --name db-fingerflitzer-$UserName
+ 
+$WebApp = az webapp show `
+  --name wa-fingerflitzer-$UserName `
+  --resource-group rg-fingerflitzer | ConvertFrom-Json
+Write-Host "### Web app: https://$($WebApp.defaultHostName)"
+ 
+$WebAppStagingSlot = az webapp deployment slot list `
+  --resource-group rg-fingerflitzer `
+  --name wa-fingerflitzer-$UserName `
+  --query "[?name=='staging']" | ConvertFrom-Json
+
+$ReplyUrls = @(
+  "https://localhost/signin-oidc"
+  "https://$($WebApp.defaultHostName)/signin-oidc"
+  "https://$($WebAppStagingSlot.defaultHostName)/signin-oidc"
+)
+$AppRegistration = az ad app create --display-name Fingerflitzer-$UserName `
+  --web-redirect-uris $ReplyUrls | ConvertFrom-Json
+
+$AppSecret = az ad app credential reset --id $AppRegistration.id `
+  --display-name Dev `
+  --append | ConvertFrom-Json
+
+az webapp config appsettings set `
+  --settings "AzureAd__ClientId=$($AppRegistration.appId)" "AzureAd__ClientSecret=$($AppSecret.password)" `
+  --slot staging `
+  --name wa-fingerflitzer-$UserName `
+  --resource-group rg-fingerflitzer
+    
+az webapp config appsettings set `
+  --settings "AzureAd__ClientId=$($AppRegistration.appId)" "AzureAd__ClientSecret=$($AppSecret.password)" `
+  --name wa-fingerflitzer-$UserName `
+  --resource-group rg-fingerflitzer
+
+dotnet user-secrets --project .\HTLVBFingerflitzer.Web\ set AzureAd:ClientSecret $($AppSecret.password)
+
+Write-Host "### Web app: https://$($WebApp.defaultHostName)"
+Write-Host "### Web app staging slot: https://$($WebAppStagingSlot.defaultHostName)"
+
+Write-Host "### App registration:"
+Write-Host "* Client id: $($AppSecret.appId)"
+Write-Host "* Tenant id: $($AppSecret.tenant)"
+Write-Host "* Client secret: $($AppSecret.password)"
+
+<#
+az group delete --name rg-fingerflitzer --no-wait
+$ServicePrincipal = az ad sp list --display-name "gh-action-to-deploy-fingerflitzer-webapp-$UserName" `
+  | ConvertFrom-Json
+az ad sp delete --id $ServicePrincipal.id
+#>
